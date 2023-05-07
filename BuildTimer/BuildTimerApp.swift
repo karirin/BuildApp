054713d164a188fc2c01e49eaeb81aaf34a1e7b1
karirin <karirin419@gmail.com>
2023-05-08 00:14:24 +0900

    Initial Commit

---
+++ BuildTimer/BuildTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTimerApp.swift
+//  BuildTimer
+//
+//  Created by hashimo ryoya on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct BuildTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
